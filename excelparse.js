9bd34eb9f1a1915ca12144697f9036d6db77fd8d
Jake <jake.woodlee@acgov.org>
2019-04-26 15:18:01 -0700

    version 1 of excelparse.js

---
+++ excelparse.js
@@ -0,0 +1,27 @@
+let Excel = require('exceljs');
+let workbook = new Excel.Workbook();
+
+workbook.xlsx.readFile(process.argv[2])
+    .then(function() {
+        let worksheet = workbook.getWorksheet(1);
+        let apns = worksheet.getColumn(1).values;
+        apns.shift();
+        apns.shift();
+        console.log(apns);
+
+        for (let i = 0; i < apns.length; i++) {
+            apns[i] = apns[i].replace(/-/g, '');
+        }
+        console.log(apns);
+
+        //let row = worksheet.getRow(5);
+
+        // row.getCell(1).value = 5; // A5's value set to 5
+        //row.commit();
+        return workbook.xlsx.writeFile(process.argv[2]);
+    });
+
+
+
+
+// console.log(process.argv[2]);
