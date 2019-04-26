a8412ea6930632cb5acd157ac3548dddca347e85
Jake <jake.woodlee@acgov.org>
2019-04-26 15:36:25 -0700

    the modified data now spawns a new column to the left

--- excelparse.js
+++ excelparse.js
@@ -4,16 +4,18 @@ let workbook = new Excel.Workbook();
 workbook.xlsx.readFile(process.argv[2])
     .then(function() {
         let worksheet = workbook.getWorksheet(1);
-        let apns = worksheet.getColumn(1).values;
+        let apns = worksheet.getColumn(1).values.slice(0);
+
         apns.shift();
-        apns.shift();
+        // apns.shift();
+
         console.log(apns);
 
         for (let i = 0; i < apns.length; i++) {
             apns[i] = apns[i].replace(/-/g, '');
         }
-        console.log(apns);
 
+        worksheet.getColumn(2).values = apns;
         //let row = worksheet.getRow(5);
 
         // row.getCell(1).value = 5; // A5's value set to 5
