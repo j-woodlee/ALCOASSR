ec91f6e4a601142b689dbd7b8a9eff53e0738330
j-woodlee <jake.woodlee@gmail.com>
2019-05-01 12:30:54 -0700

    scanning for ids

--- pdfparserpdf2json.js
+++ pdfparserpdf2json.js
@@ -1,11 +1,36 @@
-let fs = require('fs'),
-        PDFParser = require("pdf2json");
+let fs = require('fs');
+let PDFParser = require("pdf2json");
 
     let pdfParser = new PDFParser();
 
     pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
     pdfParser.on("pdfParser_dataReady", pdfData => {
-      console.log(JSON.stringify(pdfParser.getAllFieldsTypes()));
+      let json = pdfParser.getAllFieldsTypes();  //  array of objects
+
+      for (let i = 0; i < json.length; i++) { // sloppy but w/e
+          if (json[i].id === "EMPLOYEE_NAME") {
+              console.log(json[i].value);
+          }
+
+          if (json[i].id === "EMPLOYEE_ID") {
+              console.log(json[i].value);
+          }
+
+          if (json[i].id === "DATES_OF_LEAVE") {
+              console.log(json[i].value);
+          }
+
+          if (json[i].id === "DATE_OF_REQUEST") {
+              console.log(json[i].value);
+          }
+
+          if (json[i].id === "SUPERVISORS_NAME") {
+              console.log(json[i].value);
+          }
+      }
+
+
+      // console.log(JSON.stringify(json));
         // fs.writeFile("./test.json", JSON.stringify(pdfParser.getAllFieldsTypes()), () => {});
     });
 
